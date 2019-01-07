404f55933d62e8371f08e81493936c5c14af13a4
Alexander <alex.gavriliak@gmail.com>
2019-01-07 21:48:16 +0200

    Fixed

--- XTest/SystematicCode/HemingaCode/FormHemingaTest1.Designer.cs
+++ XTest/SystematicCode/HemingaCode/FormHemingaTest1.Designer.cs
@@ -28,109 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.TitleTask = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.result = new System.Windows.Forms.TextBox();
+            this.BtnNext = new System.Windows.Forms.Button();
+            this.CheckingResultLabel = new System.Windows.Forms.Label();
+            this.Check = new System.Windows.Forms.Button();
+            this.ShowAnswer = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // TitleTask
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 112);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(135, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Закодируйте сообщение:";
+            this.TitleTask.AutoSize = true;
+            this.TitleTask.Location = new System.Drawing.Point(58, 127);
+            this.TitleTask.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.TitleTask.Name = "TitleTask";
+            this.TitleTask.Size = new System.Drawing.Size(265, 25);
+            this.TitleTask.TabIndex = 0;
+            this.TitleTask.Text = "Закодируйте сообщение:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(33, 138);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(62, 177);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.Size = new System.Drawing.Size(144, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "11010010000";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(35, 173);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(66, 244);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.Size = new System.Drawing.Size(121, 25);
             this.label3.TabIndex = 2;
             this.label3.Text = "Результат:";
             // 
-            // textBox1
+            // result
             // 
-            this.textBox1.Location = new System.Drawing.Point(37, 198);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(177, 20);
-            this.textBox1.TabIndex = 3;
+            this.result.Location = new System.Drawing.Point(70, 292);
+            this.result.Margin = new System.Windows.Forms.Padding(4);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(350, 31);
+            this.result.TabIndex = 3;
             // 
-            // button1
+            // BtnNext
             // 
-            this.button1.Location = new System.Drawing.Point(159, 252);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(77, 37);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Далее";
-            this.button1.UseVisualStyleBackColor = true;
+            this.BtnNext.Location = new System.Drawing.Point(370, 657);
+            this.BtnNext.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnNext.Name = "BtnNext";
+            this.BtnNext.Size = new System.Drawing.Size(154, 71);
+            this.BtnNext.TabIndex = 4;
+            this.BtnNext.Text = "Далее";
+            this.BtnNext.UseVisualStyleBackColor = true;
+            this.BtnNext.Click += new System.EventHandler(this.BtnNext_Click);
             // 
-            // label4
+            // CheckingResultLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(17, 252);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 13);
-            this.label4.TabIndex = 5;
+            this.CheckingResultLabel.AutoSize = true;
+            this.CheckingResultLabel.Location = new System.Drawing.Point(118, 567);
+            this.CheckingResultLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CheckingResultLabel.Name = "CheckingResultLabel";
+            this.CheckingResultLabel.Size = new System.Drawing.Size(0, 25);
+            this.CheckingResultLabel.TabIndex = 5;
+            this.CheckingResultLabel.UseMnemonic = false;
             // 
-            // button2
+            // Check
             // 
-            this.button2.Location = new System.Drawing.Point(78, 252);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(76, 37);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Проверка";
-            this.button2.UseVisualStyleBackColor = true;
+            this.Check.Location = new System.Drawing.Point(208, 657);
+            this.Check.Margin = new System.Windows.Forms.Padding(4);
+            this.Check.Name = "Check";
+            this.Check.Size = new System.Drawing.Size(152, 71);
+            this.Check.TabIndex = 6;
+            this.Check.Text = "Проверка";
+            this.Check.UseVisualStyleBackColor = true;
+            this.Check.Click += new System.EventHandler(this.btnCheck_Click);
             // 
-            // metroLabel1
+            // ShowAnswer
             // 
-            this.metroLabel1.AutoSize = true;
-            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
-            this.metroLabel1.Location = new System.Drawing.Point(34, 60);
-            this.metroLabel1.Name = "metroLabel1";
-            this.metroLabel1.Size = new System.Drawing.Size(108, 25);
-            this.metroLabel1.TabIndex = 7;
-            this.metroLabel1.Text = "Кодировние";
+            this.ShowAnswer.Location = new System.Drawing.Point(48, 656);
+            this.ShowAnswer.Margin = new System.Windows.Forms.Padding(4);
+            this.ShowAnswer.Name = "ShowAnswer";
+            this.ShowAnswer.Size = new System.Drawing.Size(152, 71);
+            this.ShowAnswer.TabIndex = 7;
+            this.ShowAnswer.Text = "Ответ";
+            this.ShowAnswer.UseVisualStyleBackColor = true;
+            this.ShowAnswer.Click += new System.EventHandler(this.ShowAnswer_Click);
             // 
             // FormHemingaTest1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(283, 312);
-            this.Controls.Add(this.metroLabel1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(566, 763);
+            this.Controls.Add(this.ShowAnswer);
+            this.Controls.Add(this.Check);
+            this.Controls.Add(this.CheckingResultLabel);
+            this.Controls.Add(this.BtnNext);
+            this.Controls.Add(this.result);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Controls.Add(this.TitleTask);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormHemingaTest1";
-            this.Padding = new System.Windows.Forms.Padding(15, 60, 15, 16);
+            this.Padding = new System.Windows.Forms.Padding(30, 115, 30, 31);
             this.Resizable = false;
             this.Style = MetroFramework.MetroColorStyle.Silver;
             this.Text = "Код Хемминга";
@@ -141,13 +145,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label TitleTask;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button2;
-        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button BtnNext;
+        private System.Windows.Forms.Label CheckingResultLabel;
+        private System.Windows.Forms.Button Check;
+        private System.Windows.Forms.Button ShowAnswer;
     }
 }
